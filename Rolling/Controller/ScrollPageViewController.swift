51cecf7f78c92c4eb07783c5116c1e1bccd0969d
yukeyMonash <wangyikun1993@gmail.com>
2020-07-08 00:25:21 +1000

    scroll

--- Rolling/Controller/ScrollPageViewController.swift
+++ Rolling/Controller/ScrollPageViewController.swift
@@ -11,18 +11,22 @@ import UIKit
 class ScrollPageViewController: UIViewController {
     var newImageList = [UIImage]()
     var name: String?
+    var timer = Timer()
+    var scrollViewHeight = CGFloat(0.0)
+    lazy var height = scrollView.bounds.size.height
+    var count = 1
 
+    @IBOutlet weak var play: UIBarButtonItem!
     @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         adjustImage()
-        scrollView.delegate = self
         self.title = name
+        isShowed()
     }
     
     private func adjustImage() {
-        var scrollViewHeight = CGFloat(0.0)
         for (_, image) in newImageList.enumerated() {
             let imageView = UIImageView(image: image)
             let scale = image.size.height / image.size.width
@@ -32,8 +36,29 @@ class ScrollPageViewController: UIViewController {
         }
         scrollView.contentSize = CGSize(width: view.frame.size.width, height: scrollViewHeight)
     }
+    
+    private func isShowed() {
+        if scrollViewHeight < self.view.frame.height {
+            self.play.isEnabled = true
+        }
+    }
+    
+    @IBAction func playButton(_ sender: UIBarButtonItem) {
+        DispatchQueue.main.async {
+            self.timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(self.scrollDocument), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func scrollDocument() {
+        if count < Int(scrollViewHeight / scrollView.bounds.size.height) {
+            let scroll = CGPoint(x: 0, y: scrollView.bounds.size.height * CGFloat(count))
+            scrollView.setContentOffset(scroll, animated: false)
+            count += 1
+        } else {
+            let bottomOffset = CGPoint(x: 0, y: scrollView.contentSize.height - scrollView.bounds.size.height)
+            scrollView.setContentOffset(bottomOffset, animated: false)
+            count = 0
+        }
+    }
 }
 
-extension ScrollPageViewController: UIScrollViewDelegate {
-    
-}
